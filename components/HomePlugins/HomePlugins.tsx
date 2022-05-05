8535a76e376c94ccab8f7ebfe1be7e3a7a90f935
dongxuny <50768414+dongxuny@users.noreply.github.com>
2022-05-05 16:18:43 +0800

    Change plugin URL

--- components/HomePlugins/HomePlugins.tsx
+++ components/HomePlugins/HomePlugins.tsx
@@ -10,103 +10,103 @@ export default function HomePlugins() {
       <LogoContainer>
         <LogoRow>
           <ClickableLogo
-            href="https://github.com/grpc/grpc"
+            href="https://github.com/rookie-ninja/rk-grpc"
             src="/images/grpc.svg"
             alt="gRpc"
           />
           <ClickableLogo
-            href="https://github.com/gin-gonic/gin"
+            href="https://github.com/rookie-ninja/rk-gin"
             src="/images/gin.svg"
             alt="Gin"
           />
           <ClickableLogo
-            href="https://github.com/labstack/echo"
+            href="https://github.com/rookie-ninja/rk-echo"
             src="/images/echo.svg"
             alt="Echo"
           />
           <ClickableLogo
-            href="https://github.com/gogf/gf"
+            href="https://github.com/rookie-ninja/rk-gf"
             src="/images/goframe.svg"
             alt="GoFrame"
           />
           <ClickableLogo
-            href="https://github.com/gofiber/fiber"
+            href="https://github.com/rookie-ninja/rk-fiber"
             src="/images/fiber.svg"
             alt="Fiber"
           />
           <ClickableLogo
-            href="https://github.com/zeromicro/go-zero"
+            href="https://github.com/rookie-ninja/rk-zero"
             src="/images/go-zero.svg"
             alt="Zero"
           />
           <ClickableLogo
-            href="https://github.com/gorilla/mux"
+            href="https://github.com/rookie-ninja/rk-mux"
             src="/images/mux.svg"
             alt="Mux"
           />
         </LogoRow>
         <LogoRow>
           <ClickableLogo
-            href="https://www.mysql.com/"
+            href="https://github.com/rookie-ninja/rk-db/tree/master/mysql"
             src="/images/mysql.svg"
             alt="MySql"
           />
           <ClickableLogo
-            href="https://www.sqlite.org/index.html"
+            href="https://github.com/rookie-ninja/rk-db/tree/master/sqlite"
             src="/images/sqlite.svg"
             alt="SQLite"
           />
           <ClickableLogo
-            href="https://www.microsoft.com/en-us/sql-server"
+            href="https://github.com/rookie-ninja/rk-db/tree/master/sqlserver"
             src="/images/sqlserver.svg"
             alt="SQL Server"
           />
           <ClickableLogo
-            href="https://www.postgresql.org/"
+            href="https://github.com/rookie-ninja/rk-db/tree/master/postgres"
             src="/images/postgresql.svg"
             alt="PostgreSQL"
           />
           <ClickableLogo
-            href="https://clickhouse.com/"
+            href="https://github.com/rookie-ninja/rk-db/tree/master/clickhouse"
             src="/images/clickhouse.svg"
             alt="ClickHouse"
           />
           <ClickableLogo
-            href="https://www.mongodb.com/"
+            href="https://github.com/rookie-ninja/rk-db/tree/master/mongodb"
             src="/images/mongodb.svg"
             alt="MongoDB"
           />
           <ClickableLogo
-            href="https://redis.io/"
+            href="https://github.com/rookie-ninja/rk-db/tree/master/redis"
             src="/images/redis.svg"
             alt="Redis"
           />
         </LogoRow>
         <LogoRow>
           <ClickableLogo
-            href="https://aws.amazon.com/"
+            href="https://github.com/rookie-ninja/rk-cloud/tree/master/aws"
             src="/images/aws.svg"
             alt="AWS"
           />
           <ClickableLogo
-            href="https://intl.cloud.tencent.com/zh/"
+            href="https://github.com/rookie-ninja/rk-cloud/tree/master/tencent"
             src="/images/tencentcloud.svg"
             alt="Tencent Cloud"
           />
         </LogoRow>
         <LogoRow>
           <ClickableLogo
-            href="https://github.com/spf13/viper"
+            href="https://github.com/rookie-ninja/rk-entry"
             src="/images/viper.svg"
             alt="Viper"
           />
           <ClickableLogo
-            href="https://github.com/uber-go/zap"
+            href="https://github.com/rookie-ninja/rk-entry"
             src="/images/zap.svg"
             alt="Zap"
           />
           <ClickableLogo
-            href="https://github.com/openssl/openssl"
+            href="https://github.com/rookie-ninja/rk-entry"
             src="/images/tls.svg"
             alt="TLS"
           />
